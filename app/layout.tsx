47a204a486a4c5dd870df381ed90bd689411d5cf
Berkay BAL <berkaybal2@outlook.com>
2026-08-30 16:05:57 +0300

    Fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,18 @@ export const metadata: Metadata = {
     },
   ],
 
+  icons: {
+    icon: [
+      {
+        url: "/icon.png",
+        type: "image/png",
+      },
+    ],
+    shortcut: "/icon.png",
+    apple: "/icon.png",
+  },
+
+
 
   openGraph: {
     title: "Berkay Bal | Frontend Developer",
